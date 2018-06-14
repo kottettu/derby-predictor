0e3055a5f7c4a614f7450425ba7a205ca1314557
care0717 <dev.care0717@gmail.com>
2018-06-14 18:03:13 +0900

    feat: echoを使えるように

--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import fmt "fmt"
+import (
+	"github.com/labstack/echo"
+	"github.com/kottettu/derby-predictor/src/handler"
+)
 
 func main() {
-	fmt.Printf("Plasma")
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	//e.Use(middleware.Logger())
+	//e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", handler.MainPage())
+
+	// サーバー起動
+	e.Start(":1323")    //ポート番号指定してね
 }
